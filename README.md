622c2a65911334f77b900673260479c92ab688e7
federico-quintieri <fedequinti98@gmail.com>
2025-01-12 18:58:02 +0100

    boh

---
+++ README.md
@@ -0,0 +1,5 @@
+Consegna
+Completiamo il nostro routing con l’aggiunta delle pagine di dettaglio e della creazione del post!
+- Aggiungiamo i link alla lista dei post
+- Facciamo quindi in modo di raggiungere la pagina di dettaglio del singolo post
+- Al submit del form reindirizziamo l'utente verso la pagina di dettaglio del post creato
--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/ChiSiamo" element={<ChiSiamo />} />
             <Route path="/PostList" element={<PostList />} />
+            {/* Faccio una rotta dinamica */}
             <Route path="/PostList/:id" element={<PostDetail />}/>
           </Route>
         </Routes>
